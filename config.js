bc8d936f63527837b49adc879180df6820ff0388
Wensheng Tang <sp3478@gmail.com>
2015-06-21 19:57:51 +0800

    fix for missing config

---
+++ config.js
@@ -0,0 +1,33 @@
+
+var path = require('path');
+
+//'..' for config folder, '.' for config.js file
+var root_dir = path.resolve(__dirname, '.') + '/';
+var public_dir = root_dir + 'public/';
+
+var upload_dir = public_dir + 'data/';
+var tmp_dir = public_dir + 'data/tmp/';
+
+var dev_mode = true;
+
+var config = {
+  web: {
+    /* web server configurations */
+    address: '127.0.0.1',
+    port: '3006',
+    static_file_server: true
+  },
+
+  sys: {
+    public_dir: public_dir,
+    upload_dir: upload_dir,
+    root_dir: root_dir,
+    tmp_dir: tmp_dir
+  },
+
+  app: {
+    dev_mode: dev_mode
+  }
+};
+
+module.exports = config;
